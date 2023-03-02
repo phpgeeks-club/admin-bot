9b8697240b271c222b188fa03c5b3782cc1c7f24
DimNS <atomcms@ya.ru>
2023-03-02 18:49:29 +0500

    Removed the search inside the text

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"log"
 	"os"
-	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -32,7 +31,6 @@ func main() {
 	u.Timeout = 60 // long polling
 
 	updates := bot.GetUpdatesChan(u)
-	commands := [22]string{"/help", "/хелп", "/php", "/пхп", "/jun", "/джун", "/go", "/го", "/db", "/дб", "/lara", "/лара", "/js", "/жс", "/hr", "/хр", "/fl", "/фл", "/code", "/код", "/nometa", "/номета"}
 
 	for update := range updates {
 		if update.Message == nil {
@@ -67,11 +65,9 @@ func main() {
 			continue
 		}
 
-		for _, command := range commands {
-			if strings.Contains(update.Message.Text, command) {
-				switch command {
-				case "/help", "/хелп":
-					message = `БОТ РАБОТАЕТ ТОЛЬКО У АДМИНОВ.
+		switch update.Message.Text {
+		case "/help", "/хелп":
+			message = `БОТ РАБОТАЕТ ТОЛЬКО У АДМИНОВ.
 
 Команды можно писать обычным сообщением и ответом на сообщение.
 
@@ -85,30 +81,28 @@ func main() {
 [<code>/js</code>, <code>/жс</code>] @jsChat - Чат посвященный программированию на языке JavaScript.
 [<code>/hr</code>, <code>/хр</code>] @jobGeeks - Топ вакансии (250 000+ р/мес).
 [<code>/fl</code>, <code>/фл</code>] @freelanceGeeks - IT фриланс, ищем исполнителей и заказчиков, делимся опытом и проблемами связанными с фрилансом.
-[<code>/code</code>, <code>/код</code>] Код в нашем чате ложут на pastebin.org, gist.github.com или любой аналогичный ресурс (с)der_Igel
+[<code>/code</code>, <code>/код</code>] Код в нашем чате <a href=\"https://t.me/phpGeeks/1318040\">ложут</a> на pastebin.org, gist.github.com или любой аналогичный ресурс (с)der_Igel
 [<code>/nometa</code>, <code>/номета</code>] nometa.xyz`
-				case "/php", "/пхп":
-					message = "@phpGeeks - Best PHP chat"
-				case "/jun", "/джун":
-					message = "@phpGeeksJunior - Группа для новичков. Не стесняйтесь задавать вопросы по php."
-				case "/go", "/го":
-					message = "@golangGeeks - Приветствуем всех в нашем гетеросексуальном чате гоферов!"
-				case "/db", "/бд":
-					message = "@dbGeeks - Чат про базы данных, их устройство и приемы работы с ними."
-				case "/lara", "/лара":
-					message = "@laravel_pro - Официальный чат для всех Laravel программистов."
-				case "/js", "/жс":
-					message = "@jsChat - Чат посвященный программированию на языке JavaScript."
-				case "/hr", "/хр":
-					message = "@jobGeeks - Топ вакансии (250 000+ р/мес)."
-				case "/fl", "/фл":
-					message = "@freelanceGeeks - IT фриланс, ищем исполнителей и заказчиков, делимся опытом и проблемами связанными с фрилансом."
-				case "/code", "/код":
-					message = "Код в нашем чате ложут на pastebin.org, gist.github.com или любой аналогичный ресурс (с)der_Igel"
-				case "/nometa", "/номета":
-					message = "nometa.xyz"
-				}
-			}
+		case "/php", "/пхп":
+			message = "@phpGeeks - Best PHP chat"
+		case "/jun", "/джун":
+			message = "@phpGeeksJunior - Группа для новичков. Не стесняйтесь задавать вопросы по php."
+		case "/go", "/го":
+			message = "@golangGeeks - Приветствуем всех в нашем гетеросексуальном чате гоферов!"
+		case "/db", "/бд":
+			message = "@dbGeeks - Чат про базы данных, их устройство и приемы работы с ними."
+		case "/lara", "/лара":
+			message = "@laravel_pro - Официальный чат для всех Laravel программистов."
+		case "/js", "/жс":
+			message = "@jsChat - Чат посвященный программированию на языке JavaScript."
+		case "/hr", "/хр":
+			message = "@jobGeeks - Топ вакансии (250 000+ р/мес)."
+		case "/fl", "/фл":
+			message = "@freelanceGeeks - IT фриланс, ищем исполнителей и заказчиков, делимся опытом и проблемами связанными с фрилансом."
+		case "/code", "/код":
+			message = "Код в нашем чате <a href=\"https://t.me/phpGeeks/1318040\">ложут</a> на pastebin.org, gist.github.com или любой аналогичный ресурс (с)der_Igel"
+		case "/nometa", "/номета":
+			message = "nometa.xyz"
 		}
 
 		if message == "" {
